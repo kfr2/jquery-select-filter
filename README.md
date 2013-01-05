579ec94031270a99ac4de35a94bfd92c69a81474
Kevin Richardson <kevin@magically.us>
2013-01-04 21:33:06 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,74 @@
+# jQuery Select-Filter
+A jQuery plugin to dynamically filter long \<select\> lists through an input[type="text"] control.
+
+This plugin is forked from [a project by Andras Zoltan-Gyarfas](http:///realizare-site-web.ro/works/codes/jquery/HTML-Select-List-Filter).
+
+
+## Demo
+Forthcoming.
+
+## Getting Started
+Download the [production version]() or [development version]() of jQuery.Select-Filter.
+
+    <script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
+    <script type="text/javascript" src="jquery.select-filter.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('select.class').selectFilter();
+        });
+    </script>
+
+
+## Examples
+### Simple Syntax
+    $('select.class').selectFilter();
+
+### Verbose Syntax
+    Any of the following attributes can be inside a dictionary to `selectFilter()`.  These options highlight jQuery.select-filter's default settings.
+
+    $('select.class').selectFilter({
+        // Filtering should be case sensitive.
+        'caseSensitive': false,
+
+        // The filter box will be cleared when the user presses the Escape key.
+        'clearInputOnEscape': true,
+        
+        // If true, disable any regex the user enters into the search box.
+        'disableRegex': true,
+
+        // The class to apply to the filter bar.
+        'filterClass': 'filter-bar',
+
+        // The placeholder text for the filter bar.
+        'inputPlaceholder': 'type to filter list',
+        
+        // The minimum number of characters at which to begin the filtering process.
+        'minimumCharacters': 3,
+       
+        // The minimum size attribute (number of rows) for the element this plugin is called upon.
+        // If the element is less than this size, its size will be updated accordingly.
+        'minimumSelectElementSize': 3,
+
+        // Amount of time to delay filtering (in ms) after a key is pressed.
+        'searchDelay': 200,
+
+        // Begin searching (strictly) from the beginning of each <option> of the calling element.
+        'searchFromBeginning': false,
+
+        // The width for the select element and its input filter box.
+        // If -1, both the select element and its filter box have their size set to the width of
+        // the select element before any filtering occurs.
+        'width': -1
+    });
+
+
+## Release History
+* 2012-01-14 -- 0.0.1 -- Initial verison.
+
+
+## License
+* (GNU GPL)[http://www.gnu.org/licenses/gpl.html]
+
+
+## Authors
+* [Kevin Richardson](http://github.com/kfr2)
---
+++ jquery.select-filter.js
@@ -0,0 +1,182 @@
+/**
+ * jQuery Select-Filter 0.0.1
+ *
+ * Dynamically filter long <select> lists through an (automatically created)
+ * input[type="text"] control.
+ *
+ * https://github.com/kfr2/jquery-select-filter
+ *
+ * License: GPL - http://www.gnu.org/licenses/gpl.html
+ */
+(function($) {
+    $.fn.selectFilter = function(options) {
+        var defaults = {
+            // searching should be case sensitive
+            'caseSensitive': false,
+
+            // clear input box on Escape press
+            'clearInputOnEscape': true,
+            
+            // disable regular expressions in search input
+            'disableRegex': true,
+
+            // class to associate with filter element
+            'filterClass': 'filter-bar',
+
+            // text to display as placeholder for the filter bar
+            'inputPlaceholder': 'type to filter list',
+            
+            // the minimum number of characters required to begin initial filtering
+            'minimumCharacters': 3,
+
+            // the minimum size attribute for the select element this plugin is called on
+            'minimumSelectElementSize': 3,
+
+            // amount of time to delay filter between keypresses (in ms)
+            'searchDelay': 200,
+
+            // begin searching strickly from beginning of each option
+            'searchFromBeginning': false,
+
+            // the width for the select/input boxes. If -1, use the width of the calling element before filtering occurs
+            // the width may be numeric (in which case px will be appended) or another valid CSS designator (ex: %)
+            'width': -1
+        };
+
+        // Overwrite default settings with user provided ones.
+        options = $.extend(defaults, options);
+
+
+        // Note the calling element -- i.e., the select element -- and its
+        // children.
+        var self = this;
+        if (!self || self.get(0).tagName !== 'SELECT') {
+            return false;
+        }
+
+        // Keep a copy of the children of the calling element.
+        var clone = self.children().clone();
+
+        // Lock the calling element's width to prevent it from dynamically
+        // resizing when the maximum length of its children changes.
+        // Otherwise, the calling element may resize as its children
+        // are removed/added.
+        var width = options.width;
+        if (width === -1) {
+            width = self.width();
+        }
+
+        var widthStr = width.toString();
+        if (isFinite(widthStr)) {
+            widthStr += 'px';
+        }
+
+        self.css('width', widthStr);
+
+        
+        // Used for connection between input and select elements.
+        var name = '';
+        if (typeof(self.attr('name')) === 'undefined') {
+            name = 'undefined_name';
+        }
+        else {
+            name = self.attr('name').replace(/\]/g, '').replace(/\[/g, '');
+        }
+
+
+        // Ascertain the calling element's size is at least mininumSelectElementSize
+        if(typeof(self.attr('size')) === 'undefined' || self.attr('size') < options.minimumSelectElementSize)
+        {
+            self.attr('size', options.minimumSelectElementSize);
+        }
+
+        // Create the filter element and attach it to the calling element.
+        var filterElement = $('<input>', {
+            id: 'input_' + name,
+            type: 'text',
+            placeholder: options.inputPlaceholder,
+            class: options.filterClass,
+            style: 'display: block; width: ' + widthStr
+        });
+
+        self.addClass(name + '_select').css({'display': 'block'}).after(filterElement);
+
+
+        // Clear the input box when escape is pressed.
+        if (options.clearInputOnEscape) {
+            $('#input_' + name).on('keydown', function(key) {
+                if (key.which === 27) {
+                    $(this).val('');
+                }
+            });
+        }
+
+
+        /**
+         * Filtering logic
+         */
+        var keyDelayTimeout;
+        var filteringStarted = false;
+        var oldSearchText = null;
+
+        $('#input_' + name).on('keyup', function() {
+            clearTimeout(keyDelayTimeout);
+
+            var searchText = $(this).val();
+            if (!filteringStarted && searchText.length < options.minimumCharacters) {
+                return;
+            }
+
+            filteringStarted = true;
+
+            // Add timeout to filtering for performance reasons.
+            keyDelayTimeout = setTimeout(function() {
+                // Refresh the list of items if the search string has
+                // grown shorter as the user is backtracking.
+                // TODO: it might be neat to consider caching copies of results
+                // for various phrases and swap them into place as the user
+                // backtracks and enters previous queries.
+                if (oldSearchText !== null) {
+                    if (searchText.length <= oldSearchText.length) {
+                        self.children().remove();
+                        self.append(clone);
+                    }
+                }
+                oldSearchText = searchText;
+
+                if (!options.caseSensitive) {
+                    searchText = searchText.toLowerCase();
+                }
+
+                if (options.disableRegex) {
+                    searchText = searchText.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+                }
+
+                if (options.searchFromBeginning) {
+                    searchText = '^' + searchText;
+                }
+
+                var pattern = new RegExp(searchText);
+                self.children().each(function(i, selected) {
+                    var option_value = $(this).text();
+                    if (!options.caseSensitive) {
+                        option_value = option_value.toLowerCase();
+                    }
+
+                    if (!pattern.test(option_value)) {
+                        $(selected).remove();
+                    }
+                });
+
+                if (self.children().length === 0) {
+                    self.append('<option>(No results.)</option>');
+                }
+
+                clearTimeout(keyDelayTimeout);
+            }, options.searchDelay);
+        });
+
+
+        return this;
+    };
+})(jQuery);
